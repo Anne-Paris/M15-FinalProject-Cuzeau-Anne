bc0549cdd6cd6b934a48f79ed038989009bbb6dd
oem123 <acuzeau@stedwards.edu>
2022-04-21 11:51:46 -0500

    Initial Commit

---
+++ src/test/java/com/company/M15FinalProjectCuzeauAnne/M15FinalProjectCuzeauAnneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.M15FinalProjectCuzeauAnne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M15FinalProjectCuzeauAnneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
